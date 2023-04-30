2e85deaa2f6685569893b9a099de91d125b45216
Vaidik Rungta <rungtavaidik@gmail.com>
2023-04-30 22:38:02 +0530

    Update welcomer.js
    Started using emojis for a better look

--- client/commands/welcomer.js
+++ client/commands/welcomer.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
+const emojis = require("../emojis.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,35 +7,38 @@ module.exports = {
         .setDescription('Manage the welcome module for this server'),
     async execute(interaction) {
         const embed = new EmbedBuilder()
-            .setTitle('Welcome Module')
+            .setTitle(`${emojis.happy} Welcome Module`)
             .setColor('Random')
             .addFields(
                 {
-                    name: "Channel", value: "#channel"
+                    name: `${emojis.channel} **Channel**`, value: "#channel"
                 },
                 {
-                    name: "AutoRoles", value: "@Newbies"
+                    name: `${emojis.at} **Auto Roles**`, value: "@Newbies"
                 },
                 {
-                    name: "Message", value: "Welcome {user} to the server!"
+                    name: `${emojis.message} **Message**`, value: "Welcome {user} to the server!"
                 }
             )
-            .setDescription('Give your new users a great welcome! Use the buttons below to configure the welcome module.');
+            .setDescription('**Give your new users a great welcome! Use the buttons below to configure the welcome module.**');
 
         const settings = new ButtonBuilder()
             .setCustomId('settings')
             .setLabel('Manage Settings')
             .setStyle(ButtonStyle.Primary)
+            .setEmoji(emojis.settings)
 
         const disable = new ButtonBuilder()
             .setCustomId('disable')
             .setLabel('Disable Module')
             .setStyle(ButtonStyle.Danger)
+            .setEmoji(emojis.disable)
 
         const reset = new ButtonBuilder()
             .setCustomId('reset')
             .setLabel('Reset settings to default')
             .setStyle(ButtonStyle.Secondary)
+            .setEmoji(emojis.reset)
 
         const row = new ActionRowBuilder()
             .addComponents(settings, disable, reset);
@@ -68,18 +72,30 @@ module.exports = {
                .setPlaceholder('Type the greeting message for newcomers')
                .setValue('Welcome {user} to the server!');
 
+               const autoRoles = new TextInputBuilder()
+               .setCustomId('autoRoles')
+               .setLabel(`Auto Roles`)
+               .setStyle(TextInputStyle.Short)
+               .setRequired(true)
+               .setPlaceholder('Type the roles to be assiged for newcomers')
+
                const row1 = new ActionRowBuilder().addComponents(channelList)
                const row2 = new ActionRowBuilder().addComponents(welcomeMessage)
+               const row3 = new ActionRowBuilder().addComponents(autoRoles)
 
-               modal.addComponents(row1, row2);
+               modal.addComponents(row1, row2, row3);
                await confirmation.showModal(modal);
             } else if (confirmation.customId === 'disable') {
                 await response.edit({content: "Disabled", components: []})
-            } else if (confirmation.customId === 'resey') {
+            } else if (confirmation.customId === 'reset') {
                 await response.edit({content: "Reset", components: []})
             }
         } catch (e) {
-            await console.log(e);
+            await response.edit({
+                content: `No activity detected for 1 minute`,
+                components: [],
+                embeds: []
+            });
         }
     },
 }
